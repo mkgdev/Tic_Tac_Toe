c8beda727f44407bebeba95e68125da73dfea8e1
mkgdev <mukeshinhere07@gmail.com>
2017-07-19 13:47:15 +0530

    Code simplified

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -11,22 +11,21 @@ def display_board(board):
     print(board[0], '|', board[1], '|', board[2])
 
 
+
 def player_input():
-    print('Player 1 : Do you want "X" or "O"?')
-    player1 = raw_input()
-    while player1 != 'X' and player1 != 'O':
-        print('Please choose only "X" or "O"')
-        player1 = raw_input()
-    if player1 == 'X':
-        player2 = 'O'
+    print('Player1 : Do you want "X" or "O"?')
+    player1= raw_input().upper()
+
+    while not(player1=='X' or player1=='O'):
+        print('Please enter the correct mark')
+        player1 = raw_input().upper()
+
+    if player1=='X':
+        return ('X','O')
     else:
-        player2 = 'X'
-
-    print('Player1 chose', player1)
-    print('Player2 chose', player2)
+        return ('O','X')
 
 
-    return [player1, player2]  # returning the list of marker of players
 
 
 def place_marker(board, marker, position):
@@ -37,9 +36,9 @@ def place_marker(board, marker, position):
 def choose_first():  # Assign first turn
     draw = random.randint(0, 1)  # generate random number either '0' or '1'
     if draw == 0:
-        return ['player1', 'player2']
+        return 'Player 1'
     else:
-        return ['player2', 'player1']
+        return 'Player 2'
 
 
 def space_check(board, position):  # Check if chosen position is empty or not
@@ -50,12 +49,14 @@ def space_check(board, position):  # Check if chosen position is empty or not
 
 
 def full_board_check(board):  # Check if board is still empty or not
-    for item in board:
-	print(item)
-        if item == ' ':
-            return True
+    for index in range(0,9):
 
-    return False
+        if space_check(board,index):
+            return False
+
+
+
+    return True   
 
 
 def win_check(board, mark):
@@ -91,6 +92,7 @@ def win_check(board, mark):
 
                 # check diagonally for only %2==0
             if index % 2 == 0:
+                
                 i = index
 
                 # if position is 0
@@ -112,80 +114,86 @@ def win_check(board, mark):
 
     return False
 
+def player_choice(board):
+
+    print('Enter the position 1-9')
+
+    position = int(raw_input())
+
+    while not (position in range(1,10)):
+        print('Please enter the position 1-9 only')
+        position = int(raw_input())
+    return position
+
 
 def replay():
-    print('Enter y to continue, n to exit')
-    ans = raw_input()
-    if ans == 'y':
-        return True
-    else:
-        return False
+    print('Wanna replay? Enter "y" to continue "n" to exit')
+    return raw_input().lower().startswith('y')
+  
 
 
 print('Welcome to Tic Tac Toe Game!')
 
 
-while replay():
+while True:
 
     board = [" "] * 9
-    markerList = player_input()  # Player 1 choice return to marker
+    player1, player2 = player_input()  # getting  elements from tuples
+    
     display_board(board)
-    player1 = markerList[0]  # saving the player1 marker
-    player2 = markerList[1]  # saving the player2 marker
 
     turn = choose_first()
-    if (turn[0] == 'player2'):  # swap function
-        temp = player2
-        player2 = player1
-        player1 = temp
+    print(turn +' will go first')
+    game_on=True
 
-
-    while True:
-        print('Enter your position 1-9', turn[0])
-        pos = int(input())
-
-        # check for available spaces
-        while space_check(board, pos-1) == False:
-
-            print('position already filled! Enter position again')
-            pos = int(input())
-        else:
-
-            place_marker(board, player1, pos-1)
-
-        # check for win
-        if win_check(board, player1):
-            print(turn[0], 'win')
-            break
-
-	 # check if still space available
-        if full_board_check(board)==False:
-            # if available
-            print('Match draw')
-            break
-
-        print('Enter your position 1-9', turn[1])
-        pos = int(input())
-
-        # check for avaiable spaces
-
-        while space_check(board, pos-1) == False:
-
-            print('position already filled! Enter position again')
-            pos = int(input())
-
-        else:
-            place_marker(board, player2, pos-1)
-        # check for win
-        if win_check(board, player2):
-            print(turn[1], 'win')
-            break
-
-       
-
-    print('Wanna  replay? Enter y/n')
+    while game_on:
     
-  
+        if turn =='Player 1':
+            print('Player 1')
+            pos = player_choice(board)
+            while not(space_check(board,pos-1)):
+                print('Position already filled Please enter again ')
+                pos= int(raw_input())
+            else:
+                place_marker(board,player1,pos-1)
 
 
+            if win_check(board,player1):
+                print('Congratulation',turn,'Win')
 
+                game_on=False
+
+            if full_board_check(board):
+                print('Match draw')
+                game_on=False
+                break
+
+            turn = 'Player 2'
+    
+        else:
+          
+            if turn=='Player 2':
+
+
+                print('Player 2')
+                pos = player_choice(board)
+                while not(space_check(board,pos-1)):
+                    print('Position already filled Please enter again ')
+                    pos= int(raw_input())
+                else:
+                    place_marker(board,player2,pos-1)
+
+
+                if win_check(board,player2):
+                    print('Congratulation',turn,'Win')
+                    game_on=False
+
+                if full_board_check(board):
+                    print('Match draw')
+                    game_on=False
+                    
+                turn = 'Player 1'
+
+    if not replay():
+        print('Thanks for playing')
+        break
